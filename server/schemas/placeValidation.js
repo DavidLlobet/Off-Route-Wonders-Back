b7b69d8f984bdbad2cbb639ffb46fd46e83ced47
David <david.llobet6@gmail.com>
2021-11-23 11:58:46 +0100

    Create placeValidation with Joi

---
+++ server/schemas/placeValidation.js
@@ -0,0 +1,19 @@
+const { Joi } = require("express-validation");
+
+const placeValidation = {
+  body: Joi.object({
+    title: Joi.string()
+      .regex(/.{1,50}/)
+      .required(),
+    date: Joi.object().required,
+    country: Joi.string().required,
+    images: Joi.object().required,
+    text: Joi.string()
+      .regex(/.{50, 300}/)
+      .required(),
+    map: Joi.object().required,
+    comments: Joi.object(),
+  }),
+};
+
+module.exports = placeValidation;
