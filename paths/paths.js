96e7672e4ee850f63966dde170ee629dd2d80153
David <david.llobet6@gmail.com>
2021-11-23 10:49:18 +0100

    Create paths file

---
+++ paths/paths.js
@@ -0,0 +1,10 @@
+const paths = {
+  getAllPlaces: "/",
+  getPlacesByCountry: "/:country",
+  getPlaceById: "/:id",
+  createPlace: "/create",
+  deletePlace: "/delete/:id",
+  updatePlace: "/update",
+};
+
+module.exports = paths;
