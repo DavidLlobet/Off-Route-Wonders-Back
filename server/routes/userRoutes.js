9b65ea52143adfee4425367c8ec6871884ecadb1
David <david.llobet6@gmail.com>
2021-11-25 13:17:16 +0100

    Add getUserById route and create tests for this controller

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,10 +1,15 @@
 const express = require("express");
 
-const { userLogin, userSignUp } = require("../controllers/userControllers");
+const {
+  userLogin,
+  userSignUp,
+  getUserById,
+} = require("../controllers/userControllers");
 
 const router = express.Router();
 
 router.post("/login", userLogin);
 router.post("/register", userSignUp);
+router.get("/:id", getUserById);
 
 module.exports = router;
