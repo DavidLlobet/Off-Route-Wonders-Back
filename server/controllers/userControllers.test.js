06867b64a29893c3e30a76ebf71d03f6fdf49805
David <david.llobet6@gmail.com>
2021-11-25 12:33:41 +0100

    Create tests for userLogin controller

---
+++ server/controllers/userControllers.test.js
@@ -0,0 +1,95 @@
+require("dotenv").config();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const User = require("../../database/models/user");
+const { userLogin, userSignUp } = require("./userControllers");
+
+jest.mock("../../database/models/user");
+jest.mock("bcrypt");
+jest.mock("jsonwebtoken");
+
+const mockResponse = () => {
+  const res = {};
+  res.status = jest.fn().mockReturnValue(res);
+  res.json = jest.fn().mockReturnValue(res);
+  return res;
+};
+
+describe("Given a userLogin function", () => {
+  describe("When it receives a request with an incorrect username", () => {
+    test("Then it should invoke a next function with an error", async () => {
+      const usernameTest = "Mario";
+      const req = {
+        body: {
+          username: usernameTest,
+        },
+      };
+      const res = {};
+
+      User.findOne = jest.fn().mockResolvedValue(false);
+      const error = new Error("Wrong credentials");
+      error.code = 401;
+      const next = jest.fn();
+
+      await userLogin(req, res, next);
+
+      expect(User.findOne).toHaveBeenCalled();
+      expect(next).toHaveBeenCalledWith(error);
+      expect(next.mock.calls[0][0]).toHaveProperty("code", error.code);
+    });
+  });
+  describe("When it receives a request with a correct username and an incorrect password", () => {
+    test("Then it should invoke a next function with an error", async () => {
+      const req = {
+        body: {
+          username: "Buleano",
+          password: "Wrong password",
+        },
+      };
+      const res = {};
+      const next = jest.fn();
+
+      User.findOne = jest.fn().mockResolvedValue({
+        username: "Buleano",
+        password: "Buleano",
+      });
+
+      bcrypt.compare = jest.fn().mockResolvedValue(false);
+
+      const error = new Error("Wrong password");
+      error.code = 401;
+
+      await userLogin(req, res, next);
+
+      expect(next).toHaveBeenCalledWith(error);
+      expect(next.mock.calls[0][0]).toHaveProperty("code", error.code);
+    });
+  });
+  describe("When it receives a request with a correct username and password", () => {
+    test("Then it should respond with the method json and an object containing a token", async () => {
+      const req = {
+        body: {
+          username: "Buleano",
+          password: "Buleano",
+        },
+      };
+      const res = {
+        json: jest.fn(),
+      };
+      User.findOne = jest.fn().mockResolvedValue({
+        username: "Buleano",
+        password: "Buleano",
+      });
+      bcrypt.compare = jest.fn().mockResolvedValue(true);
+      const expectedToken = "mano";
+      jwt.sign = jest.fn().mockReturnValue(expectedToken);
+      const expectedResponse = {
+        token: expectedToken,
+      };
+
+      await userLogin(req, res);
+
+      expect(res.json).toHaveBeenCalledWith(expectedResponse);
+    });
+  });
+});
